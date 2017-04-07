d8adbf89f0b01c5fd1522f7283c9d2cbc2c58cfe
sadjad ghanbary nasab <sghgh1996@gmail.com>
2017-04-07 18:46:42 +0430

    biirectional initialized but left complete

---
+++ src/solutions/BidirectionalSearch.java
@@ -0,0 +1,17 @@
+package solutions;
+
+import problems.Problem;
+import transition_model.Node;
+
+public class BidirectionalSearch extends SearchMethod {
+
+	public BidirectionalSearch() {
+		super();
+	}
+
+	@Override
+	public Node doSearch(Problem problem, String type) {
+		return null;
+	}
+
+}
